b327ee5d32a116ba2c2d8dc5887c271387708271
sanjayagihan <73861147+sanjayagihan@users.noreply.github.com>
2022-06-29 04:55:49 +0530

    add comments

--- sendDiscord.py
+++ sendDiscord.py
@@ -2,9 +2,15 @@ import requests
    
 def sendMessage(token,channel_id,message):
     url = "https://discord.com/api/v9/channels/{}/messages".format(channel_id)
+      
+    #set content and authorization
     data = {"content": message}
     header = {"authorization": token}
+    
+    #send the message using post method and store the status in variable 'r'
     r = requests.post(url,data=data,headers=header)
+   
+    #to check the status
     print(r)
     
 
