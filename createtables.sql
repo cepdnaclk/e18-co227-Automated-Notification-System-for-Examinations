6a3297849c10663df133dd6e8df44e5f571aecb3
sanjayagihan <73861147+sanjayagihan@users.noreply.github.com>
2022-06-29 04:34:06 +0530

    add discord channel id to lecturer table

--- createtables.sql
+++ createtables.sql
@@ -18,6 +18,7 @@ CREATE TABLE `lecturer` (
   `Title` char(10),
   `Email_address` char(50),
   `Telephone_number` char(15),
+  `channel_id` char(100),
   PRIMARY KEY (`Id`)
 );
 
@@ -47,7 +48,8 @@ CREATE TABLE `Paper` (
 INSERT INTO DEPARTMENT(Department_id,Department_name,Department_head_id,Email_Address,Telephone_number) VALUES(1,'Computer Engineering Department',1,'ce@eng.pdn.ac.lk','+94xxxxxxxxx');             
 -- change the email address in here --
 
-INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number) VALUES (1,1,'Kamalanath Samarakoon','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx');
-INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number) VALUES (2,1,'Sithumini Ekanayake','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx');
-INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number) VALUES (3,1,'Swarnalatha Radhakrishnan','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx');
-INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number) VALUES (4,1,'Manjula Sandirigama','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx');
+INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number,channel_id) VALUES (1,1,'Kamalanath Samarakoon','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx','xxxxxxxxxxxxxxxxxx');
+INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number,channel_id) VALUES (2,1,'Sithumini Ekanayake','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx','xxxxxxxxxxxxxxxxxx');
+INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number,channel_id) VALUES (3,1,'Swarnalatha Radhakrishnan','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx','xxxxxxxxxxxxxxxxxx');
+INSERT INTO LECTURER(Id,Department_id,Name,Title,Email_address,Telephone_number,channel_id) VALUES (4,1,'Manjula Sandirigama','Dr','sanjayagihandesilva@gmail.com','+94xxxxxxxxx','xxxxxxxxxxxxxxxxxx');
+
