c5a4d3a102045fd66b4c341c449681cdb34284bc
sanjayagihan <73861147+sanjayagihan@users.noreply.github.com>
2022-06-29 10:54:08 +0530

    Update mailTest.py

--- mailTest.py
+++ mailTest.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 import configparser
 
 #read the configuration file and get all the connections
-config = configparser.Configparser()
+config = configParser.Configparser()
 config.read('config.ini')
 
 def sendRemainder(receiver_address, subject,msg):
